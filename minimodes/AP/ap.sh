0a8ae17c221b09b79f6dcce7a0ecb5aca2378076
akukisopo <37295716+akukisopo@users.noreply.github.com>
2018-03-28 00:37:41 +0700

    dikit

--- minimodes/AP/ap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sh /minipwner/minimodes/Lights-Out/Lights-Out.sh
-
-cp -f /minipwner/minimodes/AP/network /etc/config/network
-cp -f /minipwner/minimodes/AP/wireless /etc/config/wireless
-
-wifi
--- minimodes/Lights-Out/Lights-Out.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo 0 > /sys/devices/platform/leds-gpio/leds/tp-link:green:3g/brightness
-echo 0 > /sys/devices/platform/leds-gpio/leds/tp-link:green:wlan/brightness
-echo 0 > /sys/devices/platform/leds-gpio/leds/tp-link:green:lan/brightness 
